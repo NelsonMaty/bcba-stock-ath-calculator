d5850ed7c4fe6b821c9c7f41950d6a030cebcc3f
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-02-19 14:58:13 -0300

    feat: Add new stocks to STOCKS_DATA and streamline run.sh setup

--- run.sh
+++ run.sh
@@ -8,14 +8,13 @@ if [ ! -d "$VENV_DIR" ]; then
   echo "Virtual environment not found. Setting it up..."
   python3 -m venv $VENV_DIR
   source $VENV_DIR/bin/activate
+  echo "Installing required packages..."
+  pip install -r requirements.txt
 else
   # If the virtual environment exists, just activate it
   echo "Activating virtual environment..."
   source $VENV_DIR/bin/activate
 fi
-
-echo "Installing required packages..."
-pip install -r requirements.txt
 
 # Pass any arguments (flags) received by the script to main.py
 echo "Running main.py with the provided arguments..."
